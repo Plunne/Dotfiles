0032ff0e8ba6a6ec4cd26d1c97dff5e49eba7921
Plunne <plunne.celeste@gmail.com>
2024-02-26 20:35:50 +0100

    ZSHCFG-240101 : [ALIAS] Update Zsh Plunne aliases

---
+++ .config/zsh/pkg_debian.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+#     ___  __   __  ___  ___  ______  ____  ______ __
+#    / _ \/ /  / / / / |/ / |/ / __/ /_  / / __/ // /
+#   / ___/ /__/ /_/ /    /    / _/    / /__\ \/ _  / 
+#  /_/  /____/\____/_/|_/_/|_/___/   /___/___/_//_/  
+#
+
+# Apt
+alias agi='sudo apt-get install'
+alias agu='sudo apt-get update'
+alias agug='sudo apt-get upgrade'
+alias agr='sudo apt-get remove'
+alias agar='sudo apt-get autoremove'
+alias ags='sudo apt-get search'
+alias agl='sudo apt-get list'
